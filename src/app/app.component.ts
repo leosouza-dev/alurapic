08e92dd8e424d205738a72c949b8c7fd3e42ce77
sarival <sarival@sarival.com.br>
2020-06-01 22:49:02 -0300

    Interpolação e Property Binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'Alurapic';
+  description = 'ATM'
+  url = 'https://images.unsplash.com/photo-1539622287262-61e066a2c534?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80'
 }
