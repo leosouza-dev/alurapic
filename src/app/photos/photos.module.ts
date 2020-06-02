b9158dc7e51c2a5261a212a3d85fa8e817779d57
sarival <sarival@sarival.com.br>
2020-06-02 15:16:18 -0300

    Organizando ainda mais nossa aplicação

--- src/app/photos/photos.module.ts
+++ src/app/photos/photos.module.ts
@@ -1,13 +1,13 @@
-import { NgModule } from "@angular/core";
-import { PhotoComponent } from "./photo/photo.component";
-import { HttpClientModule } from "@angular/common/http";
+import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+
+import { PhotoComponent } from './photo/photo.component';
+import { PhotoListComponent } from './photo-list/photo-list.component';
 
 @NgModule({
   declarations:[ 
-    PhotoComponent 
-  ],
-  exports: [
-    PhotoComponent
+    PhotoComponent, 
+    PhotoListComponent 
   ],
   imports: [
     HttpClientModule
