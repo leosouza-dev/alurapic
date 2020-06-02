31393ddcbdb9b862673a226b5162d5ada6effb02
sarival <sarival@sarival.com.br>
2020-06-01 23:19:10 -0300

    Criando primeiro componente

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,4 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Alurapic';
-  description = 'ATM'
-  url = 'https://images.unsplash.com/photo-1539622287262-61e066a2c534?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80'
 }
---
+++ src/app/photo/photo.component.ts
@@ -0,0 +1,10 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: 'ap-photo',
+  templateUrl: 'photo.component.html'
+})
+export class PhotoComponent{
+  description = 'ATM'
+  url = 'https://images.unsplash.com/photo-1539622287262-61e066a2c534?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80'
+}
