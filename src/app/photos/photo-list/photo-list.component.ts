43f5de7f5a70988949328b6251dc74ea1ed53020
sarival <sarival@sarival.com.br>
2020-06-02 17:07:29 -0300

    Segmentos de rotas - parametros

--- src/app/photos/photo-list/photo-list.component.ts
+++ src/app/photos/photo-list/photo-list.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 
 import { PhotoService } from '../photo/photo.service';
 import { Photo } from '../photo/photo';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-photo-list',
@@ -12,12 +13,18 @@ export class PhotoListComponent implements OnInit {
 
   photos: Photo[] = [];
 
-  constructor(private photoService: PhotoService) {
-  }
+  constructor(
+    private photoService: PhotoService, 
+    // precisamos do activatedRoute para acessar o parametro da rota (flavio, almeida, etc)
+    private activatedRoute: ActivatedRoute) {}
   
   ngOnInit(): void{
+
+    // buscando o valor do parametro da rota
+    const userName = this.activatedRoute.snapshot.params.userName
+
     this.photoService
-      .listFromUser('flavio')
+      .listFromUser(userName)
       .subscribe(photos => this.photos = photos);
   }
 
